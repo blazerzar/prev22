d5977f97dc52986a647284387d9c8bb474c6c3f7
Blaž Erzar <blaz.erzar@gmail.com>
2022-04-14 23:35:03 +0200

    Finished intermediate code linearization

--- src/prev/phase/imclin/ExprCanonizer.java
+++ src/prev/phase/imclin/ExprCanonizer.java
@@ -11,4 +11,75 @@ import prev.data.imc.visitor.*;
  */
 public class ExprCanonizer implements ImcVisitor<ImcExpr, Vector<ImcStmt>> {
 
+    public ImcExpr visit(ImcBINOP binOp, Vector<ImcStmt> stmts) {
+        // Save left expression into temp
+        MemTemp lhs = new MemTemp();
+        stmts.add(new ImcMOVE(
+            new ImcTEMP(lhs), binOp.fstExpr.accept(this, stmts)));
+
+        // Save right expression into temp
+        MemTemp rhs = new MemTemp();
+        stmts.add(new ImcMOVE(
+            new ImcTEMP(rhs), binOp.sndExpr.accept(this, stmts)));
+
+        // Save result into temp
+        MemTemp result = new MemTemp();
+        stmts.add(new ImcMOVE(
+            new ImcTEMP(result), new ImcBINOP(
+                binOp.oper, new ImcTEMP(lhs), new ImcTEMP(rhs)
+            )
+        ));
+
+        // Return result value
+        return new ImcTEMP(result);
+    }
+
+    public ImcExpr visit(ImcCALL call, Vector<ImcStmt> stmts) {
+        // Save arguments into temporary variables
+        Vector<ImcExpr> args = new Vector<>();
+        for (ImcExpr arg : call.args) {
+            MemTemp argTemp = new MemTemp();
+            stmts.add(new ImcMOVE(
+                new ImcTEMP(argTemp), arg.accept(this, stmts)
+            ));
+            args.add(new ImcTEMP(argTemp));
+        }
+
+        // Save result into temp and return it
+        MemTemp result = new MemTemp();
+        stmts.add(new ImcMOVE(
+            new ImcTEMP(result), new ImcCALL(
+                call.label, call.offs, args
+            )
+        ));
+        return new ImcTEMP(result);
+    }
+
+    public ImcExpr visit(ImcCONST constant, Vector<ImcStmt> stmts) {
+        return new ImcCONST(constant.value);
+    }
+
+    public ImcExpr visit(ImcMEM mem, Vector<ImcStmt> stmts) {
+        // Make sure addr is in canonical form
+        return new ImcMEM(mem.addr.accept(this, stmts));
+    }
+
+    public ImcExpr visit(ImcNAME name, Vector<ImcStmt> stmts) {
+        return new ImcNAME(name.label);
+    }
+
+    public ImcExpr visit(ImcSEXPR sExpr, Vector<ImcStmt> stmts) {
+        // Canonize all stmts
+        stmts.addAll(sExpr.stmt.accept(new StmtCanonizer(), null));
+        return sExpr.expr.accept(this, stmts);
+    }
+
+    public ImcExpr visit(ImcTEMP temp, Vector<ImcStmt> stmts) {
+        return new ImcTEMP(temp.temp);
+    }
+
+    public ImcExpr visit(ImcUNOP unOp, Vector<ImcStmt> stmts) {
+        return new ImcUNOP(unOp.oper, unOp.subExpr.accept(this, stmts));
+    }
+
 }
--- src/prev/phase/imclin/StmtCanonizer.java
+++ src/prev/phase/imclin/StmtCanonizer.java
@@ -13,4 +13,79 @@ import prev.data.imc.visitor.*;
  */
 public class StmtCanonizer implements ImcVisitor<Vector<ImcStmt>, Object> {
 
+    public Vector<ImcStmt> visit(ImcCJUMP cjump, Object arg) {
+        Vector<ImcStmt> stmts = new Vector<>();
+
+        // Canonize expression and add it to stmts
+        stmts.add(new ImcCJUMP(
+            cjump.cond.accept(new ExprCanonizer(), stmts),
+            cjump.posLabel, cjump.negLabel
+        ));
+
+        return stmts;
+    }
+
+    public Vector<ImcStmt> visit(ImcESTMT eStmt, Object arg) {
+        Vector<ImcStmt> stmts = new Vector<>();
+
+        // Canonize expression and add it to stmts
+        stmts.add(new ImcESTMT(eStmt.expr.accept(new ExprCanonizer(), stmts)));
+        return stmts;
+    }
+
+    public Vector<ImcStmt> visit(ImcJUMP jump, Object arg) {
+        return new Vector<>(Arrays.asList(new ImcStmt[]{
+            new ImcJUMP(jump.label)
+        }));
+    }
+
+    public Vector<ImcStmt> visit(ImcLABEL label, Object arg) {
+        return new Vector<>(Arrays.asList(new ImcStmt[]{
+            new ImcLABEL(label.label)
+        }));
+    }
+
+    public Vector<ImcStmt> visit(ImcMOVE move, Object arg) {
+        Vector<ImcStmt> stmts = new Vector<>();
+
+        // Canonize destination and source and save them into temps
+        MemTemp dstTemp = new MemTemp();
+
+        if (move.dst instanceof ImcMEM) {
+            // Save addr to temp
+            stmts.add(new ImcMOVE(
+                new ImcTEMP(dstTemp),
+                ((ImcMEM) move.dst.accept(new ExprCanonizer(), stmts)).addr
+            ));
+
+            // Store to memory address
+            stmts.add(new ImcMOVE(
+                new ImcMEM(new ImcTEMP(dstTemp)),
+                move.src.accept(new ExprCanonizer(), stmts)
+            ));
+        } else if (move.dst instanceof ImcTEMP) {
+            // Store in temporary variable
+            stmts.add(new ImcMOVE(
+                move.dst.accept(new ExprCanonizer(), stmts),
+                move.src.accept(new ExprCanonizer(), stmts)
+            ));
+        } else {
+            // Error, this should not happen
+            throw new Report.InternalError();
+        }
+
+        return stmts;
+    }
+
+    public Vector<ImcStmt> visit(ImcSTMTS stmts, Object arg) {
+        Vector<ImcStmt> statements = new Vector<>();
+
+        // Canonize every statement
+        for (ImcStmt stmt : stmts.stmts) {
+            statements.addAll(stmt.accept(this, null));
+        }
+
+        return statements;
+    }
+
 }
